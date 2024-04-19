92ece5dc9e9fc570bde22c5e9134ef1bac33c959
VitorHMelo <135380294+VitorHMelo@users.noreply.github.com>
2024-04-19 08:06:46 -0300

    Add files via upload

---
+++ conexao.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+class Conexao():
+    def conectar():
+        mydb = mysql.connector.connect(
+            host="10.110.140.166",
+            user="equipe",
+            password="123456789",
+            database="CRABIZ"
+            )
+        
+        return mydb
